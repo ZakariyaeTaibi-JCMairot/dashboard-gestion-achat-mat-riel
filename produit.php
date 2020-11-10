eda4a375bf70c133415196cc505bd8938a8a3583
jc mairot <j.mairot@codeur.online>
2020-11-10 12:19:22 +0100

    commit de la page produit

--- produit.php
+++ produit.php
@@ -2,7 +2,30 @@
 // insertion du code php
 // gérer l'accés à la table des produits
 ?>
+<!-- navigation sticky pour rendre plus facile l'accés aux modules dashboard -->
+<nav>
+	<ul>
+		<li><a href="">Tableau des produits</a></li>
+		<li><a href="">Modifier un produit</a></li>
+		<li><a href="">Ajout d'un produit</a></li>
+	</ul>
+</nav>
 
+<h1>DASHBOARD BACKOFFICE</h1>
+
+<h2>PRODUITS</h2>
+<!-- tableau read la base de donné pour afficahge dans un tableau / bouton suppr/ 
+bouton motifier qui renvoie à un formulaire prés rempli 
+avec les données du produit à modifier-->
+
+
+<h2>MODIFIER UN PRODUIT</h2>
+<!-- formulaire comme ajout mais prérempli en fonction du bouton modifier 
+du tableau produit. ajout d'un select avec le nom et/ou ref du produit boucle 
+foreach dans le menu select + un aucun produit selectionné par defaut-->
+
+<h2>AJOUT D'UN PRODUIT</h2>
+<!--formulaire d'ajout de produit-->
 
 <!-- Pour chaque produit je doit rentrer les informations suivantes
 	- Lieux d'achat (En vente directe ou e-commerce)
