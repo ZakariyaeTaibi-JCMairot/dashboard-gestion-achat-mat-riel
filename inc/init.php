f24767c28b5bfe90fe17e3a16f3be0c89e21e3dd
jc mairot <j.mairot@codeur.online>
2020-11-12 12:23:08 +0100

    read bdd pour fiche produit

--- inc/init.php
+++ inc/init.php
@@ -1,11 +1,18 @@
 <?php
-// lien vers la base de données
+// ouverture de la session
+session_start();
 
+// lien vers la base de données
 $pdo= new PDO('mysql:host=localhost;dbname=crudzak','root','',array(
 	PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
 	PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,	
 	PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"
 ));
+//variable de controle et affichage des messages d'erreur 
+$error='';
 
+
+// FONCTIONS
+include('fonctions.php');
 
 ?>
--- produit.php
+++ produit.php
@@ -1,60 +1,69 @@
 <?php
+// accés à la base de donnée
+require_once('inc/init.php');
+//variable de la requete sql
+$sqlSelect='SELECT * FROM `produitzak`';
+// variable preparatiuon de la requête
+$query = $pdo->prepare($sqlSelect);
+// execution de la requête
+$query->execute();
+$resultats=$query->fetchAll();
+
+
+
+
+
 require_once('inc/header.php');
 require_once('inc/nav.php');
-require_once('inc/init.php');
-// insertion du code php
-// gérer l'accés à la table des produits
-
-
-
 ?>
 <div class="bgDashBoard">
 	<h1 class="titleDashboard">DASHBOARD BACKOFFICE</h1>
 	<!-- tableau read la base de donné pour afficahge dans un tableau / bouton suppr/ 
 	bouton motifier qui renvoie à un formulaire prés rempli 
 	avec les données du produit à modifier-->
+	
 	<div class="allProduit" id="allProduit">
 		<h2 class="h2Produit">PRODUITS</h2>
-		<div id="cardProduit" class="ficheProduit">
-			<!-- entête de la fiche produit -->
-			<div class="headProduit">
-				<div>
-					<h2>Nom du produit</h2>
+		<!-- debut foreach read produitzak=>produit-->
+		<?php foreach($resultats as $result): extract($result) ?>
+			<div id="cardProduit" class="ficheProduit">
+				<!-- entête de la fiche produit -->
+				<div class="headProduit">
+					<div>
+						<h2><?=$nom_prod?> <?=$ref_prod?></h2>
+					</div>
+					<div >
+						<span class="badge badge-secondary"><?=$cate_prod?></span>
+					</div>			
 				</div>
-				<div>
-					<h3>reférence du produit</h3>
+				<hr>
+				<!-- corps de la fich produit -->
+				<div class="mainProduit">
+					<div>
+						<p><?=$conseil_util?></p> 
+						
+					</div>
+					<div class="div-vide"><img src="" alt=""></div> 
+					<div class="adminProduit ">
+						<button class="btn btn-dark btn-block">Modifier</button>
+						<button class="btn btn-dark btn-block">Supprimer</button>
+						<button class="btn btn-dark btn-block">Documentation</button>
+					</div>
 				</div>
-				<div >
-					<span class="badge badge-secondary">categorie</span>
-				</div>			
+				<hr>
+				<!-- footer de l fiche produit -->
+				<div class="footerProduit">
+					<div><?=$lieux_achat?></div>
+					<div>Date d'achat : <?=$date_achat?></div>
+					<div>Date fin de garantie : <?=$fin_garantie?></div>
+					<div class="prixProduit"><?=$prix_prod." €"?></div>
+				</div>
 			</div>
-			<hr>
-			<!-- corps de la fich produit -->
-			<div class="mainProduit">
-				<div>
-					<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-					Molestiae, tempora aliquam. Omnis blanditiis ipsa similique
-					illum itaque libero nostrum voluptatem amet ut! Molestias, 
-					repudiandae. Vitae maiores fugit id a dolor!</p> 
-					
-				</div>
-				<div class="div-vide"><img src="" alt=""></div> 
-				<div class="adminProduit ">
-					<button class="btn btn-dark btn-block">Modifier</button>
-					<button class="btn btn-dark btn-block">Supprimer</button>
-					<button class="btn btn-dark btn-block">Documentation</button>
-				</div>
-			</div>
-			<hr>
-			<!-- footer de l fiche produit -->
-			<div class="footerProduit">
-				<div>Adresse achat</div>
-				<div>Date d'achat</div>
-				<div>Date de fin de garantie</div>
-				<div class="prixProduit">Prix</div>
-			</div>
-		</div>
+		<!-- fin foreach read produitzak=>produit-->
+		<?php endforeach ?>
+
 	</div>
+<!-- update produit -->
 	<div class="upDateProduit bg-light" id="upDateProduit">
 		<h2>MODIFIER LE PRODUIT : code php nom produit</h2>
 		<form class="readProduit" method="POST">
@@ -63,9 +72,6 @@ require_once('inc/init.php');
 			<option>php tous les nom de produit avec la ref</option>
 		</select>
 		</form>
-		<!-- formulaire comme ajout mais prérempli en fonction du bouton modifier 
-		du tableau produit. ajout d'un select avec le nom et/ou ref du produit boucle 
-		foreach dans le menu select + un aucun produit selectionné par defaut-->
 		<div class="formUpDate">
 		<form method="POST">
 			<!-- nom du produit / reference du produit / categorie  -->
@@ -129,6 +135,7 @@ require_once('inc/init.php');
 			</form>
 		</div>
 	</div>
+
 	<!--formulaire d'ajout de produit-->
 
 	<div class="createProduit" id="createProduit">
